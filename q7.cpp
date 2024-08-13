528cac966075c1fc57c9319acb7ad190a09876fd
Sayan-Ghosh-5 <161219150+Sayan-Ghosh-5@users.noreply.github.com>
2024-08-13 10:56:41 +0530

    Add files via upload

---
+++ q7.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+template <typename T>
+T findMax(T a, T b, T c) {
+    T max = a; 
+    if (b > max) {
+        max = b;
+        }
+
+    if (c > max) {
+        max = c; 
+    }
+
+    return max; 
+}
+
+int main() {
+   
+    int a = 5, b = 9, c = 3;
+    std::cout << "The largest integer is: " << findMax(a, b, c) << std::endl;
+
+    double x = 5.7, y = 9.1, z = 3.4;
+    std::cout << "The largest double is: " << findMax(x, y, z) << std::endl;
+
+    char p = 'a', q = 'z', r = 'm';
+    std::cout << "The largest character is: " << findMax(p, q, r) << std::endl;
+
+    return 0;
+}
